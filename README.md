005b7603ecbdfb43d84aee45e5b0b4a0d1a0c926
Anton Glazunov <kampel2012@yandex.ru>
2023-06-24 12:41:03 +0500

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# coffeeKOTELOV
-coffeeKOTELOV
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <div className="container mx-auto text-3xl">Hello world</div>;
+}
+
+export default App;
